8adb7767c6ef790ab1cda1bf2036c00c337ffa51
Das <maccameron@live.ca>
2019-12-07 00:50:08 +0100

    autocomplete search widget

--- index.js
+++ index.js
@@ -14,7 +14,20 @@ const fetchData = async (searchTerm) => {
     return res.data.Search
 }
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+<label><b>Search For A Movie</b></label>
+      <input class="input" />
+      <div class="dropdown>
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+      </div>
+    </div>
+`
+
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = debounce(async event => {
     const movies = await fetchData(event.target.value)
