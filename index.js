c1403250e92b536c6b646d706d3bcb94278b15ab
Das <maccameron@live.ca>
2019-12-07 14:56:55 +0100

    bothsided autocomplete

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+const autocompleteConfig = {
     renderOption(movie) {
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         return `
@@ -28,6 +27,16 @@ createAutoComplete({
 
         return res.data.Search
     }
+}
+
+createAutoComplete({
+    ...autocompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+})
+
+createAutoComplete({
+    ...autocompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
 })
 
 const onMovieSelect = async movie => {
